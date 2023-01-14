19e598d686c0e7c568512a74cbaf3d70353eb811
Marceswan <Marc.e.swan@gmail.com>
2023-01-14 10:54:44 -0700

    Update Code.tsx

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -5,11 +5,6 @@ import theme from 'prism-react-renderer/themes/oceanicNext';
 import { siteConfig } from '~/config/site.config';
 import { Language } from '~/types/codeBlock';
 import { CodeCopyButton } from './CodeCopyButton';
-//import Prism from "prism-react-renderer/prism";
-
-//(typeof global !== "undefined" ? global : window).Prism = Prism;
-
-require("prismjs/components/prism-apex");
 
 type CodeProps = {
   codeString: string;
